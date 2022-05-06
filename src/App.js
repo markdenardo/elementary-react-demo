8d2a67ff87087c8996a1386c282b0d2bb3ae03fc
markdenardo <mark.denardo@gmail.com>
2022-05-06 12:19:08 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
-import Sine from "./Sine";
+import {
+  ElementaryWebAudioRenderer as core,
+  el,
+} from "@nick-thompson/elementary";
+
+core.on("load", function () {
+  core.render(el.cycle(440), el.cycle(441));
+});
+
+(async function main() {
+  let node = await core.initialize(ctx, {
+    numberOfInputs: 0,
+    numberOfOutputs: 1,
+    outputChannelCount: [2],
+  });
+
+  node.connect(ctx.destination);
+})();
 
 function App() {
-  return (
-    <div className="App">
-      <Sine />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/Sine.js
+++ src/Sine.js
@@ -1,11 +1,19 @@
-import { ElementaryNodeRenderer as core, el } from "@elemaudio/core-lite";
+import { ElementaryNodeRenderer as core, el } from "@nick-thompson/elementary";
 
 function Sine() {
   core.on("load", function () {
     core.render(el.cycle(440), el.cycle(441));
   });
 
-  core.initialize();
+  (async function main() {
+    let node = await core.initialize(ctx, {
+      numberOfInputs: 0,
+      numberOfOutputs: 1,
+      outputChannelCount: [2],
+    });
+
+    node.connect(ctx.destination);
+  })();
 }
 
 export default Sine;
